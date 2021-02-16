3e7c57403a35f06214d2f2f7d46ad5b6a6a6f093
JimmyHuang <1902161621@qq.com>
2021-02-17 07:43:48 +0800

    update

--- python/client_main.py
+++ python/client_main.py
@@ -1,20 +1,40 @@
 # -*- coding: UTF-8 -*-
-from loguru import logger
 from ECY import rpc
 import ECY.engines.engines as engines
+from loguru import logger
+import argparse
+import os
 import sys
 
-path = "C:/Users/qwer/Desktop/vimrc/myproject/ECY/RPC/EasyCompleteYou2/ECY_Debug_log/ECY_debug.log"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(BASE_DIR) + '/engines')
 
-sys.path.append('C:/Users/qwer/Desktop/vimrc/myproject/ECY/RPC/EasyCompleteYou2/engines')
+#######################################################################
+#                                flags                                #
+#######################################################################
+parser = argparse.ArgumentParser(
+    description='EasyCompleteYou, Easily complete you.')
+parser.add_argument('--debug_log', action='store_true', help='debug with log')
+parser.add_argument('--log_dir', help='the file of log to output')
+g_args = parser.parse_args()
 
+#######################################################################
+#                                Debug                                #
+#######################################################################
 logger.remove()
-with open(path, 'w', encoding='utf-8') as f:
-    f.write('')
+if g_args.log_dir is None:
+    path = BASE_DIR + '/ECY_debug.log'
+else:
+    path = g_args.log_dir
 
-level = "DEBUG"
-# level = "CRITICAL"
-logger.add(path, level=level, encoding='utf-8')
+if g_args.debug_log:
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write('')
+    level = "DEBUG"
+    logger.add(path, level=level, encoding='utf-8')
 
+#######################################################################
+#                                main                                 #
+#######################################################################
 rpc.BlindEvent(engines.Mannager())
 rpc.Daemon()
