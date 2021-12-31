a0bcb6ef35a6c7b732b2948f61896b444161b99b
JimmyHuang <jimmyhuang454@gmail.com>
2021-12-31 23:09:40 +0800

    ok

--- test/ECY_test.py
+++ test/ECY_test.py
@@ -42,7 +42,11 @@ class Case(object):
         self.cmd = '%s -u NONE -i NONE -n -N --cmd "source %s"' % (VIM_EXE,
                                                                    vim_script)
         print(self.cmd)
-        self.pro = subprocess.Popen(self.cmd, shell=True)
+        self.pro = subprocess.Popen(self.cmd,
+                                    shell=True,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    stdin=subprocess.PIPE)
         threading.Thread(target=self.Test).start()
 
     def Test(self):
