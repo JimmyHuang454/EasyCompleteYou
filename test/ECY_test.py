98158c6e1f6db04c10afa94cd15a437b96b74587
JimmyHuang <jimmyhuang454@gmail.com>
2022-01-03 22:54:47 +0800

    all ok

--- test/ECY_test.py
+++ test/ECY_test.py
@@ -57,17 +57,11 @@ class Case(object):
         self.cmd = '%s -u NONE -i NONE -n -N --cmd "source %s"' % (VIM_EXE,
                                                                    vim_script)
         print(self.cmd)
-        if IS_NEOVIM and GetCurrentOS() == 'Windows':
-            self.pro = subprocess.Popen(self.cmd,
-                                        shell=True,
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE)
-        else:
-            self.pro = subprocess.Popen(self.cmd,
-                                        shell=True,
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE,
-                                        stdin=subprocess.PIPE)
+        self.pro = subprocess.Popen(self.cmd,
+                                    shell=True,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    stdin=subprocess.PIPE)
         threading.Thread(target=self.Test).start()
 
     def ReadLog(self):
