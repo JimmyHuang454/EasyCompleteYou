afa055e77e82f1c6dd76e143fe9793d49c181d82
JimmyHuang <jimmyhuang454@gmail.com>
2022-01-11 02:03:07 +0800

    test

--- python/ECY/lsp/stand_IO_connection.py
+++ python/ECY/lsp/stand_IO_connection.py
@@ -118,6 +118,7 @@ class Operate:
             if argvs is not None:
                 cmd += ' ' + argvs
 
+            subprocess.Popen('sudo chmod -R 755 %s' % cmd, shell=True).wait()
             logger.debug("real cmd: " + cmd)
             process_obj = subprocess.Popen(cmd,
                                            shell=True,
