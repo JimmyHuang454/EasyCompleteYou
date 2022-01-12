10a3493cb37ac0aab6d1a52f1833c3e4b9edd277
JimmyHuang <jimmyhuang454@gmail.com>
2022-01-13 01:30:56 +0800

    update vls

--- engines/ECY_engines/html/lsp/vls.py
+++ engines/ECY_engines/html/lsp/vls.py
@@ -8,7 +8,7 @@ class Operate(lsp.Operate):
     def __init__(self, engine_name):
         lsp.Operate.__init__(self,
                              engine_name,
-                             refresh_regex=r'[\w+\-]',
+                             refresh_regex=r'[\-\@\w+]',
                              languageId='html')
         self.snip = ultisnips.Operate()
 
@@ -18,61 +18,5 @@ class Operate(lsp.Operate):
 
     def OnCompletion(self, context):
         context = super().OnCompletion(context)
-        if context is None:
-            return  # server not supports.
-
-        show_list = []
-        for item in context['show_list']:
-            results_format = {
-                'abbr': '',
-                'word': '',
-                'kind': '',
-                'menu': '',
-                'info': '',
-                'user_data': ''
-            }
-
-            if 'kind' in item:
-                results_format['kind'] = self._lsp.GetKindNameByNumber(
-                    item['kind'])
-            else:
-                results_format['kind'] = 'Unknow'
-
-            item_name = item['label']
-
-            results_format['abbr'] = item_name
-            results_format['word'] = item_name
-
-            detail = []
-            if 'detail' in item:
-                detail = item['detail'].split('\n')
-                results_format['menu'] = item['detail']
-
-            document = []
-
-            if 'insertTextFormat' in item:
-                insertTextFormat = item['insertTextFormat']
-                if insertTextFormat == 2:
-                    snippet = None
-                    if 'textEdit' in item:
-                        snippet = item['textEdit']
-                        snippet = snippet['newText']
-                    elif 'insertText' in item:
-                        snippet = item['insertText']
-
-                    if snippet is not None:
-                        results_format['snippet'] = snippet
-                        results_format['kind'] += '~'
-
-            if 'documentation' in item:
-                if type(item['documentation']) is str:
-                    temp = item['documentation'].split('\n')
-                elif type(item['documentation']) is dict:
-                    temp = item['documentation']['value'].split('\n')
-
-                document.extend(temp)
-
-            results_format['info'] = '\n'.join(document)
-            show_list.append(results_format)
-        context['show_list'] = show_list
+        context = super()._to_ECY_format(context)
         return context
